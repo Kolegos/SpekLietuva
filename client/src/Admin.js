a779f302c70bb9c72dc1654cacafbe6e15c470b8
tomjor <tomasjorudas23@gmail.com>
2020-07-14 23:07:11 +0300

    admin search is functional, returns photos as well

--- client/src/Admin.js
+++ client/src/Admin.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const Admin = () => {
   const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
 
   function handleSearchClick(e) {
     e.preventDefault();
@@ -13,17 +14,42 @@ const Admin = () => {
       )
       .then((res) => {
         console.log(res.data.results);
+        setResults(res.data.results);
       });
   }
   return (
     <div>
-      <h4>Input your query</h4>
+      <h4>Įvesk užklausą</h4>
       <input
         onChange={(e) => {
           setQuery(e.target.value);
         }}
       ></input>
-      <button onClick={handleSearchClick}>Search</button>
+      <button onClick={handleSearchClick}>Ieškoti</button>
+      <div>
+        {results.map((result) => {
+          return (
+            <div>
+              {result.photos ? (
+                <>
+                  <p>Rezultato vardas {result.name}</p>
+                  <p>
+                    Rezultato nuotraukos id {result.photos[0].photo_reference}
+                  </p>
+                  <p>Rezultato vietos id {result.place_id}</p>
+                  <button>Pridėti į duomenų bazę</button>
+
+                  <div>
+                    <img
+                      src={`https://maps.googleapis.com/maps/api/place/photo?maxheight=200&maxwidth=200&photoreference=${result.photos[0].photo_reference}&key=AIzaSyAVyeg8CYlyD_D1rvejnzkhSjNjvKvSAKY`}
+                    ></img>
+                  </div>
+                </>
+              ) : null}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
