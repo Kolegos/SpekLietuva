87bf52f8bda791c48572ad77cf43e8324e35ea53
edgnav <55535002+edgnav@users.noreply.github.com>
2020-07-23 10:18:02 +0300

    final category card

--- client/src/components/CategoriesCards.js
+++ client/src/components/CategoriesCards.js
@@ -4,27 +4,14 @@ import '../App.css';
 class CategoriesCards extends Component {
     render() {
       return (
-        <div>
-          <div class="card">
+        <div class="container">
         <img src="https://cdn.britannica.com/67/19367-050-885866B4/Valley-Taurus-Mountains-Turkey.jpg" 
-        alt="Avatar"
-         />
-        <div class="container">
-          <h4><b>Mountains</b></h4>
-          <img class="cardImg" src="https://i.pinimg.com/originals/9c/6c/0d/9c6c0dbafebc090d4e28cd53cb23fe3a.jpg"/>
-       
+         alt="Avatar" class="image"/>
+            <div class="middle">
+              <input type="button" class="Mybutton" value="Lets play"></input>
+            </div>
         </div>
-        </div>
-        <div class="card">
-        <img src="https://www.testas.krikscioniskifilmai.lt/wp-content/uploads/Pretty-Church.jpg" 
-        alt="Avatar"
-         />
-        <div class="container">
-          <h4><b>Churches</b></h4>
-          <img class="cardImg" src="https://i.pinimg.com/originals/9c/6c/0d/9c6c0dbafebc090d4e28cd53cb23fe3a.jpg"/>
-        </div>
-        </div>
-        </div>
+
      
       );
     }
