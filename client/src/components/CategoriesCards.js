4380585f7d87b6dd8b760f18a39f50166ac21965
edgnav <55535002+edgnav@users.noreply.github.com>
2020-07-23 11:25:59 +0300

    Dabar jau tikrai geras cardas

--- client/src/components/CategoriesCards.js
+++ client/src/components/CategoriesCards.js
@@ -8,7 +8,8 @@ class CategoriesCards extends Component {
         <img src="https://cdn.britannica.com/67/19367-050-885866B4/Valley-Taurus-Mountains-Turkey.jpg" 
          alt="Avatar" class="image"/>
             <div class="middle">
-              <input type="button" class="Mybutton" value="Lets play"></input>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+            <button class="btn"><i class="fa fa-play"></i></button>
             </div>
         </div>
 
