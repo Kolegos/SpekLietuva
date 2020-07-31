b4842746256b199988ff888c1d08ceb6ea3c11b6
edgnav <55535002+edgnav@users.noreply.github.com>
2020-07-31 12:49:11 +0300

    evaldui pataisyt

--- client/src/components/navigation/NavBar.js
+++ client/src/components/navigation/NavBar.js
@@ -4,43 +4,27 @@ import "../../App.css";
 class NavBar extends Component{
     render(){
         return(
-            <div >
-  
-                <img class="BackroundImg"
+            <div class="BackroundImg overlay">
+                <img 
                 src="https://www.makalius.lt/wp-content/gallery/nida-galerija-6-foto-paid/nida-laikrodis-saules-13746.jpg?v=11e6b9b030871d14"/>
-                
                 <div class="containerForNav">
                 <p><button class="centeredHomePageTitle" type="button">Spėk Lietuvą!</button></p>
                 <div class="overlay"></div>
                 <div class="buttonNav">
-                
                     <a href="#"> 
                     <button class="glow-on-hover" type="button">Kategorijos</button> 
                     </a>
-
                     <a href="#"> 
                         <button class="glow-on-hover" type="button">Prisijungimas</button> 
                     </a>
-
                     <a href="#"> 
                         <button class="glow-on-hover" type="button">Registracija</button> 
                     </a>
-
                 </div>
-
-                    
                 </div>
                 
             </div>
-            /*<div class="topNavBar">
-            <b><div class="NameOfTheSite">Spėk Lietuvą</div></b>
-            <a class="red" href="/log in">Prisijungti</a>
-            <a class="green" href="#news">Turnyrinė lentelė</a>
-            <a class="yellow" href="#news">Kazkas</a>
-            <c href=""><img height="50px" width="400px" 
-            src="https://upload.wikimedia.org/wikipedia/commons/1/11/Flag_of_Lithuania.svg"/></c>
-          
-            </div>*/
+            
         )
     }
 }
