581ba2f26f99f2e66c5b2a215bd8cd109dd81a6f
Evaldas Mikalauskas <mikalauskas.e@gmail.com>
2020-07-31 13:33:57 +0300

    fixed background

--- client/src/components/navigation/NavBar.js
+++ client/src/components/navigation/NavBar.js
@@ -1,32 +1,36 @@
 import React, { Component } from "react";
 import "../../App.css";
 
-class NavBar extends Component{
-    render(){
-        return(
-            <div class="BackroundImg overlay">
-                <img 
-                src="https://www.makalius.lt/wp-content/gallery/nida-galerija-6-foto-paid/nida-laikrodis-saules-13746.jpg?v=11e6b9b030871d14"/>
-                <div class="containerForNav">
-                <p><button class="centeredHomePageTitle" type="button">Spėk Lietuvą!</button></p>
-                <div class="overlay"></div>
-                <div class="buttonNav">
-                    <a href="#"> 
-                    <button class="glow-on-hover" type="button">Kategorijos</button> 
-                    </a>
-                    <a href="#"> 
-                        <button class="glow-on-hover" type="button">Prisijungimas</button> 
-                    </a>
-                    <a href="#"> 
-                        <button class="glow-on-hover" type="button">Registracija</button> 
-                    </a>
-                </div>
-                </div>
-                
-            </div>
-            
-        )
-    }
+class NavBar extends Component {
+  render() {
+    return (
+      <div className="BackroundImg">
+        <div className="containerForNav">
+          <p>
+            <button className="centeredHomePageTitle" type="button">
+              Spėk Lietuvą!
+            </button>
+          </p>
+          <div className="">
+            <a href="#">
+              <button className="glow-on-hover" type="button">
+                Kategorijos
+              </button>
+            </a>
+            <a href="#">
+              <button className="glow-on-hover" type="button">
+                Prisijungimas
+              </button>
+            </a>
+            <a href="#">
+              <button className="glow-on-hover" type="button">
+                Registracija
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default NavBar;
-
