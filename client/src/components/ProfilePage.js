eabaf4dd14e60046e46b6b515eb1daba6244d0c7
tomjor <tomasjorudas23@gmail.com>
2020-08-06 01:04:27 +0300

    login and signup working, connected with Auth0

---
+++ client/src/components/ProfilePage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+
+const ProfilePage = () => {
+  const { user, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <h1>Palauk tevai</h1>;
+  }
+
+  const { name, picture, email } = user;
+
+  return (
+    <div>
+      <img src={picture} alt="Profile picture" style={{ maxHeight: "100px" }} />
+      <h2>{name}</h2>
+      <p>{email}</p>
+      <p>{JSON.stringify(user, null, 2)}</p>
+    </div>
+  );
+};
+
+export default ProfilePage;
