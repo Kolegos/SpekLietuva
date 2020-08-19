2a2c135696afb60e26e982c1338e0f56e1de394c
tomjor <tomasjorudas23@gmail.com>
2020-08-20 00:04:05 +0300

    equal card height, added text

--- client/src/components/CategoriesPage/CategoriesCard.js
+++ client/src/components/CategoriesPage/CategoriesCard.js
@@ -19,16 +19,23 @@ const CategoriesCards = () => {
   }, []);
 
   return (
-    <div className="parent parent-background">
+    <div className="parent">
       {categories.map((category) => (
-        <div className="box" key={category.category_id}>
+        <div
+          className="box"
+          style={{ height: window.screen.width > 1280 ? "20em" : "10em" }}
+          key={category.category_id}
+        >
+          {console.log(window.screen.height)}
           <div
             className="container"
+            style={{ height: window.screen.width > 1280 ? "19em" : "9em" }}
             key={category.category_id}
             onClick={() => {
               history.push(`/category/${category.category_id}`);
             }}
           >
+            <p>{category.name}</p>
             <img src={category.photo} alt="Avatar" className="image" />
             <div className="middle">
               <link
